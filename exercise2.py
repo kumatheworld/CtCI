ede2d288440c1c292f1fe1f3f581b9a262c4b8fa
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-01-13 19:40:21 +0900

    Implement LinkedList

--- exercise2.py
+++ exercise2.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable, Iterator
 from typing import Generic, Optional, TypeVar
 
 T = TypeVar("T")
@@ -7,3 +8,42 @@ class Node(Generic[T]):
     def __init__(self, data: T, next: Optional["Node[T]"] = None):
         self.data = data
         self.next = next
+
+
+class LinkedList(Generic[T]):
+    def __init__(self, it: Iterable[T] = ()):
+        self.head: Optional[Node[T]] = None
+        self.extend(it)
+
+    def __repr__(self) -> str:
+        return f"LL{tuple(iter(self))[::-1]}"
+
+    def __iter__(self) -> Iterator[T]:
+        node = self.head
+        while node is not None:
+            yield node.data
+            node = node.next
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, LinkedList):
+            return False
+        for d1, d2 in zip(self, other):
+            if d1 != d2:
+                return False
+        return True
+
+    def append(self, data: T) -> None:
+        node = Node(data, self.head)
+        self.head = node
+
+    def extend(self, it: Iterable[T]) -> None:
+        for data in it:
+            self.append(data)
+
+    def pop(self) -> T:
+        head = self.head
+        if head is None:
+            raise IndexError("pop from empty list")
+        data = head.data
+        self.head = head.next
+        return data
