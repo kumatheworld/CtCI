f0d21e23ed813e4598c372eb8c749fc11a237147
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-01-16 11:19:18 +0900

    2.1 done

---
+++ exercise2_1.py
@@ -0,0 +1,32 @@
+from random import choices
+from unittest import TestCase, main
+
+from exercise2 import LinkedList, T
+
+
+def solve(ll: LinkedList[T]) -> None:
+    seen = set()  # is set the best?
+    node = ll.head
+    if node is not None:
+        runner = node
+    while node is not None:
+        if node.data in seen:
+            runner.next = node.next
+        else:
+            seen.add(node.data)
+            runner = node
+        node = node.next
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        l = choices(range(10), k=20)
+        ll = LinkedList(l)
+        solve(ll)
+        s = set(l)
+        self.assertEqual(len(ll), len(s))
+        self.assertEqual(set(ll), s)
+
+
+if __name__ == "__main__":
+    main()
