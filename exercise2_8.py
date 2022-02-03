7404f886685dd8e2dec19e5a26034c25483e96d0
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-03 19:43:37 +0900

    2.8 done

---
+++ exercise2_8.py
@@ -0,0 +1,36 @@
+from random import choices, randrange
+from unittest import TestCase, main
+
+from exercise2 import LinkedList, Node, T
+
+
+def solve(x: LinkedList[T]) -> Node[T]:
+    seen = set()
+    node = x.head
+    while node is not None:
+        nid = id(node)
+        if nid in seen:
+            break
+        seen.add(nid)
+        node = node.next
+    return node
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        for _ in range(10):
+            l = randrange(1, 10)
+            x = LinkedList(choices(range(10), k=l))
+            ix = randrange(l)
+            nx = x.head
+            for _ in range(ix):
+                nx = nx.next
+            node = x.head
+            while node.next is not None:
+                node = node.next
+            node.next = nx
+            self.assertIs(solve(x), nx)
+
+
+if __name__ == "__main__":
+    main()
