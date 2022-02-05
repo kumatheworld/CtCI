dff1def9f3d27595b19c89be603f72e8f567eb67
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-05 17:36:30 +0900

    Add DoublyLinkedList

--- exercise2.py
+++ exercise2.py
@@ -62,3 +62,75 @@ class BiNode(Generic[T]):
         self.data = data
         self.prev = prev
         self.next = next
+
+
+class DoublyLinkedList(Generic[T]):
+    def __init__(self, it: Iterable[T] = ()) -> None:
+        self.head: Optional[BiNode[T]] = None
+        self.tail: Optional[BiNode[T]] = None
+        self.extend(it)
+
+    def __repr__(self) -> str:
+        return f"DLL{tuple(iter(self))[::-1]}"
+
+    def __iter__(self) -> Iterator[T]:
+        node = self.head
+        while node is not None:
+            yield node.data
+            node = node.next
+
+    def __len__(self) -> int:
+        return len([_ for _ in self])
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, LinkedList) and all(
+            x == y for x, y in zip_longest(self, other, fillvalue=object())
+        )
+
+    def append(self, data: T) -> None:
+        head = self.head
+        node = BiNode(data, next=head)
+        if head is None:
+            self.tail = node
+        else:
+            head.prev = node
+        self.head = node
+
+    def appendleft(self, data: T) -> None:
+        tail = self.tail
+        node = BiNode(data, prev=tail)
+        if tail is None:
+            self.head = node
+        else:
+            tail.next = node
+        self.tail = node
+
+    def extend(self, it: Iterable[T]) -> None:
+        for data in it:
+            self.append(data)
+
+    def extendleft(self, it: Iterable[T]) -> None:
+        for data in it:
+            self.appendleft(data)
+
+    def pop(self) -> T:
+        head = self.head
+        if head is None:
+            raise IndexError("pop from empty list")
+        data = head.data
+        self.head = head.next
+        head = self.head
+        if head is not None:
+            head.prev = None
+        return data
+
+    def popleft(self) -> T:
+        tail = self.tail
+        if tail is None:
+            raise IndexError("pop from empty list")
+        data = tail.data
+        self.tail = tail.prev
+        tail = self.tail
+        if tail is not None:
+            tail.next = None
+        return data
