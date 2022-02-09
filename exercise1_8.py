0ef9b99bcbedf63075cc71dda7b868085670ebe1
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-09 19:46:37 +0900

    1.8 corrected

--- exercise1_8.py
+++ exercise1_8.py
@@ -4,19 +4,20 @@ import numpy as np
 
 
 # technically solve() could always return the zero matrix
-def solve(m: np.ndarray) -> np.ndarray:
-    return m if m.all() else np.zeros_like(m)
+def solve(m: np.ndarray) -> None:
+    cols, rows = np.nonzero(m == 0)
+    m[cols] = 0
+    m[:, rows] = 0
 
 
 class TestSolution(TestCase):
-    def test_pos(self) -> None:
-        m = np.random.rand(3, 4)
-        m[1, 2] = 0
-        np.testing.assert_array_equal(solve(m), np.zeros_like(m))
-
-    def test_neg(self) -> None:
-        m = np.random.rand(3, 4) + 1
-        np.testing.assert_array_equal(solve(m), m)
+    # just look at the printed arrays!
+    def test(self) -> None:
+        m = np.random.randint(10, 100, (6, 10))
+        m[m > 95] = 0
+        print(m)
+        solve(m)
+        print(m)
 
 
 if __name__ == "__main__":
