1fc0e1dc6aaf5ba030a0b950b36520590a4089cd
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-10 23:23:40 +0900

    2.3 corrected

--- exercise2_3.py
+++ exercise2_3.py
@@ -1,50 +1,25 @@
 from unittest import TestCase, main
 
-from exercise2 import LinkedList, T
+from exercise2 import LinkedList, Node
 
 
-def solve(ll: LinkedList[T]) -> None:
-    node = ll.head
-    if node is None:
-        raise IndexError("pop from empty list")
-
-    node = node.next
-    if node is None:
-        ll.head = None
-        return
-
-    node = node.next
-    runner = ll.head
-    while node is not None:
-        node = node.next
-        if node is None:
-            break
-        node = node.next
-        runner = runner.next
-    runner.next = runner.next.next
+def solve(node: Node) -> None:
+    nn = node.next
+    node.data = nn.data
+    node.next = nn.next
 
 
 class TestSolution(TestCase):
     def test_pos(self) -> None:
-        for s in ("o", "you", "welcome", "to", "kumatheworld"):
+        for s in ("you", "welcome", "to", "kumatheworld"):
             ll = LinkedList(s)
-            solve(ll)
-            k = len(s)
-            m = k // 2
-            if k % 2 == 0:
-                ll_gts = (
-                    LinkedList(s[: m - 1] + s[m:]),
-                    LinkedList(s[:m] + s[m + 1 :]),
-                )
-                self.assertIn(ll, ll_gts)
-            else:
-                ll_gt = LinkedList(s[:m] + s[m + 1 :])
-                self.assertEqual(ll, ll_gt)
-
-    def test_neg(self) -> None:
-        ll = LinkedList()
-        with self.assertRaises(IndexError):
-            solve(ll)
+            m = len(s) // 2
+            node = ll.head
+            for _ in range(m, len(s) - 1):
+                node = node.next
+            solve(node)
+            ll_gt = LinkedList(s[:m] + s[m + 1 :])
+            self.assertEqual(ll, ll_gt)
 
 
 if __name__ == "__main__":
