5b067b66fc817c2e4644c0a8a78b5c07448d20ae
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-18 19:33:11 +0900

    3.1 done

---
+++ exercise3_1.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+
+class TriStackArray:
+    def __init__(self, size: int = 0) -> None:
+        p1_base = 2 * size // 3
+        self.p1_base = p1_base
+        self.p0 = 0
+        self.p1 = p1_base
+        self.p2 = p1_base + 1
+        self.size = size
+        self.array = [None] * size
+
+    def __repr__(self) -> str:
+        a = self.array
+        return (
+            f"{[a[i] for i in range(0, self.p0, 1)]}, "
+            f"{[a[i] for i in range(self.p1_base, self.p1, -1)]}, "
+            f"{[a[i] for i in range(self.p1_base + 1, self.p2, 1)]}"
+        )
+
+    def push0(self, item: Any) -> None:
+        if self.p0 > self.p1:
+            raise MemoryError("stack overflow")
+        self.array[self.p0] = item
+        self.p0 += 1
+
+    def push1(self, item: Any) -> None:
+        if self.p0 > self.p1:
+            raise MemoryError("stack overflow")
+        self.array[self.p1] = item
+        self.p1 -= 1
+
+    def push2(self, item: Any) -> None:
+        if self.p2 >= self.size:
+            raise MemoryError("stack overflow")
+        self.array[self.p2] = item
+        self.p2 += 1
+
+    def pop0(self) -> Any:
+        if self.p0 <= 0:
+            raise IndexError("pop from empty stack")
+        self.p0 -= 1
+        return self.array[self.p0]
+
+    def pop1(self) -> Any:
+        if self.p1 >= self.p1_base:
+            raise IndexError("pop from empty stack")
+        self.p1 += 1
+        return self.array[self.p1]
+
+    def pop2(self) -> Any:
+        if self.p2 <= self.p1_base + 1:
+            raise IndexError("pop from empty stack")
+        self.p2 -= 1
+        return self.array[self.p2]
