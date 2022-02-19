b07bf6082c40370c891595fd2fcfc5dac081f4fe
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-19 17:36:58 +0900

    Define comparable type CT

--- common.py
+++ common.py
@@ -1,3 +1,13 @@
-from typing import TypeVar
+from abc import ABCMeta, abstractmethod
+from typing import Any, TypeVar
 
 T = TypeVar("T")
+
+
+class Comparable(metaclass=ABCMeta):
+    @abstractmethod
+    def __lt__(self, other: Any) -> bool:
+        pass
+
+
+CT = TypeVar("CT", bound=Comparable)
