21c3384b85b78bda7705503fd7275becf8b67dd8
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-23 19:46:20 +0900

    3.3 done

---
+++ exercise3_3.py
@@ -0,0 +1,29 @@
+from collections import deque
+from typing import Generic
+
+from common import T
+
+
+class SetOfStacks(Generic[T]):
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
+        self.stacks = deque[deque[T]]([deque()])
+
+    def __repr__(self) -> str:
+        return repr(self.stacks)
+
+    def push(self, item: T) -> None:
+        stack = self.stacks[-1]
+        stack.append(item)
+        if len(stack) == self.max_size:
+            self.stacks.append(deque())
+
+    def pop(self) -> T:
+        stack = self.stacks[-1]
+        if not stack:
+            try:
+                self.stacks.pop()
+                stack = self.stacks[-1]
+            except IndexError:
+                raise IndexError("pop from empty set of stacks")
+        return stack.pop()
