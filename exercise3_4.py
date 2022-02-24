b31ec6e06064be23b9eaa533e4abb9f639f02992
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-02-24 18:46:35 +0900

    3.4 done

---
+++ exercise3_4.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Generic
+
+from common import T
+
+
+class BiStackQueue(Generic[T]):
+    def __init__(self) -> None:
+        self.s0 = deque[T]()
+        self.s1 = deque[T]()
+
+    def __repr__(self) -> str:
+        return f"(s0={self.s0}, s1={self.s1}"
+
+    def enque(self, item: T) -> None:
+        self.s0.append(item)
+
+    def deque(self) -> T:
+        s1 = self.s1
+        if not s1:
+            s0 = self.s0
+            if not s0:
+                raise IndexError("pop from empty queue")
+            while s0:
+                s1.append(s0.pop())
+        return s1.pop()
