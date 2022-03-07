d09d733e5c5af752c41cae7693aca7f0023e6238
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-03-07 20:18:40 +0900

    Add Node

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,8 @@
+from typing import Generic, Optional
+
+from common import T
+
+
 class AdjListGraph:
     def __init__(self, edges: list[set[int]]) -> None:
         self.edges = edges
@@ -6,3 +11,15 @@ class AdjListGraph:
     def from_string(cls, str_edges: str) -> "AdjListGraph":
         edges = [set(int(v) for v in s.split()) for s in str_edges.split(";")]
         return cls(edges)
+
+
+class Node(Generic[T]):
+    def __init__(
+        self,
+        data: T,
+        left: Optional["Node[T]"] = None,
+        right: Optional["Node[T]"] = None,
+    ) -> None:
+        self.data = data
+        self.left = left
+        self.right = right
