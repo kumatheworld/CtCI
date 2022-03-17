95843110ea8a8f09e180c2f20b740db0bcaaf4a1
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-03-17 21:34:35 +0900

    4.4 done

--- exercise4_4.py
+++ exercise4_4.py
@@ -1,10 +1,31 @@
 from unittest import TestCase, main
 
 from common import CT
-from exercise4 import BinarySearchTree, BinaryTree
+from exercise4 import BinarySearchTree, BinaryTree, Node
+
+
+class ImbalancedTreeException(Exception):
+    pass
+
+
+def height(node: Node[CT]) -> int:
+    left = node.left
+    right = node.right
+    hl = 0 if left is None else height(left)
+    hr = 0 if right is None else height(right)
+    diff = hl - hr
+    if diff < -1 or 1 < diff:
+        raise ImbalancedTreeException
+    return max(hl, hr) + 1
 
 
 def solve(t: BinaryTree[CT]) -> bool:
+    if t.root is None:
+        return True
+    try:
+        height(t.root)
+    except ImbalancedTreeException:
+        return False
     return True
 
 
