cc2edddc78342f9e229f1c8a74f1bb93e4eaa234
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-03-18 20:01:57 +0900

    4.5 done

---
+++ exercise4_5.py
@@ -0,0 +1,36 @@
+from unittest import TestCase, main
+
+from common import CT
+from exercise4 import BinarySearchTree, BinaryTree
+
+
+def solve(t: BinaryTree[CT]) -> bool:
+    return t.is_binary_search_tree()
+
+
+class TestSolution(TestCase):
+    def test_pos(self) -> None:
+        data = ["", "kumatheworld", "Cracking the Coding Interview"]
+        for s in data:
+            t = BinarySearchTree[str]()
+            for c in s:
+                t.insert(c)
+
+            self.assertTrue(solve(t))
+
+    def test_neg(self) -> None:
+        t = BinarySearchTree[str]()
+        for c in "kumatheworld":
+            t.insert(c)
+        t.root.right.left.right.data = "n"
+        self.assertFalse(solve(t))
+
+        t = BinarySearchTree[str]()
+        for c in "Cracking the Coding Interview":
+            t.insert(c)
+        t.root.right.left.right.right.left.left.data = "q"
+        self.assertFalse(solve(t))
+
+
+if __name__ == "__main__":
+    main()
