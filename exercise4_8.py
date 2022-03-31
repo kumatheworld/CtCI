c36a514205e1b1e47b4e58abba47e2387d315e5c
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-03-31 21:12:45 +0900

    4.8 testcases done

---
+++ exercise4_8.py
@@ -0,0 +1,53 @@
+from random import choice, randrange, sample
+from unittest import TestCase, main
+
+from common import CT
+from exercise4 import BinaryTree, Node
+
+
+def solve(x: Node[CT], y: Node[CT]) -> Node[CT]:
+    return x
+
+
+def randomize(tree: BinaryTree[int]):
+    if tree:
+        root = tree.root
+        root.data = randrange(100)
+        randomize(root.left)
+        randomize(root.right)
+
+
+def ancestors(node: Node[CT]) -> list[Node[CT]]:
+    anc = [node]
+    while node := node.parent:
+        anc.append(node)
+    return anc[::-1]
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        # for k in range(100):
+        for k in [100]:
+            l = sample(range(k), k)
+            t = BinaryTree[int]()
+            for z in l:
+                t.insert(z)
+            t.root.point_parent()
+            x = t.find(choice(l))
+            y = t.find(choice(l))
+
+            randomize(t)
+
+            ax = ancestors(x)
+            ay = ancestors(y)
+            m = min(len(ax), len(ay))
+            i = 0
+            while i < m and ax[i] == ay[i]:
+                i += 1
+            z = ax[i - 1]
+
+            self.assertEqual(solve(x, y), z)
+
+
+if __name__ == "__main__":
+    main()
