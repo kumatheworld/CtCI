5afbf354969a5deb91b46cb3e34a587c51fbab32
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-04-08 21:51:40 +0900

    Increase test repetitions

--- exercise4_10.py
+++ exercise4_10.py
@@ -12,18 +12,19 @@ def solve(t1: BinaryTree[CT], t2: BinaryTree[CT]) -> bool:
 
 class TestSolution(TestCase):
     def test_pos(self) -> None:
-        t1 = BinaryTree[bool]()
-        for _ in range(100):
-            t1.random_insert(bool(getrandbits(1)))
-        t2 = BinaryTree[bool]()
         for _ in range(10):
-            t2.random_insert(bool(getrandbits(1)))
-        node = t1.get_random_node()
-        if getrandbits(1):
-            node.left = copy(t2)
-        else:
-            node.right = copy(t2)
-        self.assertTrue(solve(t1, t2))
+            t1 = BinaryTree[bool]()
+            for _ in range(10000):
+                t1.random_insert(bool(getrandbits(1)))
+            t2 = BinaryTree[bool]()
+            for _ in range(10):
+                t2.random_insert(bool(getrandbits(1)))
+            node = t1.get_random_node()
+            if getrandbits(1):
+                node.left = copy(t2)
+            else:
+                node.right = copy(t2)
+            self.assertTrue(solve(t1, t2))
 
     def test_neg(self) -> None:
         pass
