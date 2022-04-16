11532048175051733b0464d66aaaa252c090f7c0
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-04-16 19:34:03 +0900

    4.11 done

---
+++ exercise4_11.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from unittest import TestCase, main
+
+from common import CT
+from exercise4 import BinaryTree, Node
+
+
+def solve(t: BinaryTree[CT]) -> Node[CT]:
+    return t.get_random_node()
+
+
+class TestSolution(TestCase):
+    # just look at the printed counter!
+    def test(self) -> None:
+        t = BinaryTree[str]()
+        data = "kumatheworld"
+        for c in data:
+            t.insert(c)
+        counter = Counter([solve(t).data for _ in range(12000)])
+        print(t)
+        print(counter)
+
+
+if __name__ == "__main__":
+    main()
