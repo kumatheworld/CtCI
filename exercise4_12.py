3cf178d2e078da401044ad52bdc363864fcac7c5
kumatheworld <kumatheworld1105@yahoo.co.jp>
2022-04-20 19:56:38 +0900

    4.12 improved

--- exercise4_12.py
+++ exercise4_12.py
@@ -5,32 +5,29 @@ from exercise4 import BinaryTree
 
 
 def solve(x: int, t: BinaryTree[CT]) -> int:
-    def partial_sums(t_: BinaryTree) -> tuple[list[int], list[int]]:
+    def partial_sums(t_: BinaryTree) -> tuple[list[int], int]:
         if t_:
             root = t_.root
             data = root.data
-            rl, ul = partial_sums(root.left)
-            rr, ur = partial_sums(root.right)
-            r = rl + rr
-            sums_rooted = [data + s for s in r] + [data]
-            sums_unrooted = ul + ur + r
-            return sums_rooted, sums_unrooted
+            rl, cl = partial_sums(root.left)
+            rr, cr = partial_sums(root.right)
+            sums_rooted = [data + s for s in rl + rr] + [data]
+            count = cl + cr + sums_rooted.count(x)
+            return sums_rooted, count
         else:
-            return [], []
+            return [], 0
 
-    sums_rooted, sums_unrooted = partial_sums(t)
-    sums = sums_rooted + sums_unrooted
-    return sums.count(x)
+    return partial_sums(t)[1]
 
 
 class TestSolution(TestCase):
     def test(self) -> None:
-        # This is not a test! Start the debug mode and just inspect sums
+        # This is not a test! Run this multiple times and get some sense
         t = BinaryTree[int]()
-        for i in range(7):
+        for i in range(10):
             t.insert(i)
         print(t)
-        print(solve(5, t))
+        print(solve(7, t))
 
 
 if __name__ == "__main__":
