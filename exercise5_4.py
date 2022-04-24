9de6b7ed8b9bb1af5920b53fa596330e7471ff88
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-04-24 10:58:01 +0900

    5.4 test cases fixed

--- exercise5_4.py
+++ exercise5_4.py
@@ -8,15 +8,15 @@ def solve(x: int) -> tuple[int, Optional[int]]:
 class TestSolution(TestCase):
     def test(self) -> None:
         data = [
-            (0b1, 0b10, None),
-            (0b10, 0b100, 0b1),
-            (0b11, 0b101, None),
-            (0b1011, 0b1101, 0b111),
-            (0b110010, 0b110100, 0b110001),
-            (0b101011010111, 0b101011011011, 0b101011001111),
+            (0b1, (0b10, None)),
+            (0b10, (0b100, 0b1)),
+            (0b11, (0b101, None)),
+            (0b1011, (0b1101, 0b111)),
+            (0b110010, (0b110100, 0b110001)),
+            (0b101011010111, (0b101011011011, 0b101011001111)),
         ]
-        for x, s in data:
-            self.assertEqual(solve(x), s)
+        for x, (y, z) in data:
+            self.assertEqual(solve(x), (y, z))
 
 
 if __name__ == "__main__":
