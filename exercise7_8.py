d40c0d8e1dbada8c9a1423c67cb70e83bcb2fb1b
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-06-17 21:54:59 +0900

    Add Othello

--- exercise7_8.py
+++ exercise7_8.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from enum import IntEnum
 
 
@@ -8,3 +9,19 @@ class Square(IntEnum):
 
     def __str__(self) -> str:
         return ".xo"[self]
+
+
+@dataclass
+class Othello:
+    board: list[list[Square]] = field(init=False)
+
+    def __post_init__(self) -> None:
+        board = [[Square.EMPTY] * 8 for _ in range(8)]
+        board[3][3] = Square.WHITE
+        board[3][4] = Square.BLACK
+        board[4][3] = Square.BLACK
+        board[4][4] = Square.WHITE
+        self.board = board
+
+    def __str__(self) -> str:
+        return "\n".join("".join(str(square) for square in row) for row in self.board)
