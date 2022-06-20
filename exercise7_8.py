da8d6a3d7a9d2a74e1f8b8db5a8b0c0fb016aa9d
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-06-20 22:37:12 +0900

    Add Direction

--- exercise7_8.py
+++ exercise7_8.py
@@ -26,6 +26,17 @@ class Square:
             self.state = self.state.opposite()
 
 
+class Direction(Enum):
+    E = (1, 0)
+    SE = (1, 1)
+    S = (0, 1)
+    SW = (-1, 1)
+    W = (-1, 0)
+    NW = (-1, -1)
+    N = (0, -1)
+    NE = (1, -1)
+
+
 @dataclass
 class Othello:
     board: list[list[Square]] = field(init=False, repr=False, compare=False)
