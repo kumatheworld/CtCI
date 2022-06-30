67e4a20f6cf67b58e825fee17407163f30fe0c1e
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-06-30 20:27:10 +0900

    Delete Message

--- exercise7_7.py
+++ exercise7_7.py
@@ -2,7 +2,6 @@ import selectors
 import socket
 from argparse import ArgumentParser
 from dataclasses import dataclass, field
-from datetime import datetime
 from types import SimpleNamespace
 
 
@@ -28,20 +27,10 @@ class User(CommUnit):
 
 
 @dataclass
-class Message:
-    sender: str
-    content: str
-    timestamp: datetime
-
-
-@dataclass
 class ChatServer(CommUnit):
     bufsize: int = 1024
     users: dict[tuple[str, int], str] = field(
         default_factory=dict, init=False, compare=False
-    )
-    messages: list[Message] = field(
-        default_factory=list, init=False, repr=False, compare=False
     )
 
     def run(self) -> None:
