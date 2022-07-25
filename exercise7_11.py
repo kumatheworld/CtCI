ecd917d9c8672838d56be50ab7dd384ea7e3dc18
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-07-25 23:15:49 +0900

    Add Directory.touch()

--- exercise7_11.py
+++ exercise7_11.py
@@ -6,3 +6,6 @@ class Directory(File):
     def __init__(self) -> None:
         self.files: dict[str, File] = {}
         self.dirs: dict[str, "Directory"] = {}
+
+    def touch(self, name: str) -> None:
+        self.files[name] = File()
