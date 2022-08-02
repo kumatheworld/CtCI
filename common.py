c0b6f00825785b475f01ebcfd5b041b5b730b0d8
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-08-02 19:39:19 +0900

    Use Protocol, not ABC

--- common.py
+++ common.py
@@ -1,25 +1,20 @@
-from abc import ABCMeta, abstractmethod
-from typing import Any, TypeVar
+from typing import Any, Protocol, TypeVar
 
 T = TypeVar("T")
 
 
-class Comparable(metaclass=ABCMeta):
-    @abstractmethod
+class Comparable(Protocol):
     def __ge__(self, other: Any) -> bool:
-        pass
+        ...
 
-    @abstractmethod
     def __gt__(self, other: Any) -> bool:
-        pass
+        ...
 
-    @abstractmethod
     def __le__(self, other: Any) -> bool:
-        pass
+        ...
 
-    @abstractmethod
     def __lt__(self, other: Any) -> bool:
-        pass
+        ...
 
 
 CT = TypeVar("CT", bound=Comparable)
