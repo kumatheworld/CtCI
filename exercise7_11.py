6dd0011bcedefa3ac639b0bb591f6db8b0ec979e
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-08-04 20:57:48 +0900

    Add FileSystem.cd()

--- exercise7_11.py
+++ exercise7_11.py
@@ -47,3 +47,42 @@ class FileSystem:
 
     def __str__(self) -> str:
         return self.pwd + "\n" + "\n".join(self.cwd._tree())
+
+    def cd(self, path: str) -> None:
+        if path == "-":
+            if self.owd:
+                self.cwd, self.owd = self.owd, self.owd
+                return
+            else:
+                raise ValueError("Old working directory not set")
+
+        norm_path = os.path.normpath(path)
+        if norm_path == ".":
+            self.owd = self.cwd
+            return
+
+        dirnames = norm_path.split(os.path.sep)
+        self_dirnames = self.dirnames.copy()
+        if os.path.isabs(norm_path):
+            d1r = self.root
+            del dirnames[0]
+        else:
+            d1r = self.cwd
+
+        try:
+            for name in dirnames:
+                if name == "..":
+                    if (parent := d1r.parent) is not None:
+                        d1r = parent
+                        self_dirnames.pop()
+                else:
+                    d1r = d1r.dirs[name]
+                    self_dirnames.append(name)
+        except KeyError:
+            raise FileNotFoundError(
+                f"The system cannot find the path specified: '{path}'"
+            ) from None
+
+        self.owd = self.cwd
+        self.cwd = d1r
+        self.dirnames = self_dirnames
