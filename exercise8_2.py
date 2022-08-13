a5bc6a59bf5c2534f1c573863f91c2add7a1d24c
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-08-13 22:30:08 +0900

    Define CellMatrix

--- exercise8_2.py
+++ exercise8_2.py
@@ -1,9 +1,12 @@
+from collections import UserList
 from enum import Enum, auto
+
+import numpy as np
 
 
 class Cell(Enum):
-    O = auto()
-    X = auto()
+    O = 0
+    X = 1
 
     def __str__(self) -> str:
         return super().__str__()[-1]
@@ -15,3 +18,13 @@ class Direction(Enum):
 
     def __str__(self) -> str:
         return super().__str__().split(".")[-1][0]
+
+
+class CellMatrix(UserList):
+    def __init__(self, width: int, height: int, prob_o: float):
+        self.data = [
+            [Cell(c) for c in row] for row in np.random.rand(height, width) > prob_o
+        ]
+
+    def __str__(self) -> str:
+        return "\n".join("".join(str(c) for c in row) for row in self)
