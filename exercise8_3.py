e40bf829e327325e55842f42656e32751f985f77
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-08-19 21:30:14 +0900

    8.3 done

--- exercise8_3.py
+++ exercise8_3.py
@@ -4,7 +4,21 @@ from unittest import TestCase, main
 
 
 def solve(a: list[int]) -> Optional[int]:
-    return None
+    def solve_(a_: list[int], offset: int) -> Optional[int]:
+        if a_ == []:
+            return None
+
+        m = len(a_) // 2
+        n = m + offset
+        d = a_[m] - n
+        if d < 0:
+            return solve_(a_[m + 1 :], n + 1)
+        elif d > 0:
+            return solve_(a_[:m], offset)
+        else:
+            return n
+
+    return solve_(a, 0)
 
 
 class TestSolution(TestCase):
