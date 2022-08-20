735b5b4c4e6bd7e45a6445c0946101a4365a3000
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-08-20 21:20:04 +0900

    8.4 done

---
+++ exercise8_4.py
@@ -0,0 +1,27 @@
+from itertools import chain, combinations
+from unittest import TestCase, main
+
+from common import T
+
+
+def solve(s: set[T]) -> set[frozenset[T]]:
+    return set(
+        chain.from_iterable(
+            (frozenset(t) for t in combinations(s, n)) for n in range(len(s) + 1)
+        )
+    )
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        s = "kumatheworld"
+        for i in range(len(s)):
+            pref = set(s[:i])
+            t = set(solve(pref))
+            self.assertEqual(len(t), 1 << i)
+            for u in t:
+                self.assertTrue(u.issubset(pref))
+
+
+if __name__ == "__main__":
+    main()
