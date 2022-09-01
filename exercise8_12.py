7ba10d958ab5164c1f4a001d75f46663fdb2f48c
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-09-01 21:07:30 +0900

    8.12 done

--- exercise8_12.py
+++ exercise8_12.py
@@ -3,7 +3,25 @@ from unittest import TestCase, main
 
 
 def solve(n: int = 8) -> list[tuple[int, ...]]:
-    return []
+    lists = []
+
+    def solve_(ok: list[list[int]], acc: list[int]) -> None:
+        if not ok:
+            lists.append(acc)
+            return
+
+        if not (ok_head := ok[0]):
+            return
+
+        for j in ok_head:
+            ok_tail = ok[1:]
+            for k, l in enumerate(ok_tail):
+                ok_tail[k] = [i for i in l if i not in (j - k - 1, j, j + k + 1)]
+            solve_(ok_tail, [*acc, j])
+
+    solve_([list(range(n)) for _ in range(n)], [])
+
+    return [tuple(l) for l in lists]
 
 
 class TestSolution(TestCase):
