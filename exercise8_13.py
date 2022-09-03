5046badc3c0d32fc3f66eab0a2596d8f1ff4c386
kumatheworld1105@yahoo.co.jp <kumatheworld1105@yahoo.co.jp>
2022-09-03 23:22:45 +0900

    8.13 solution done

--- exercise8_13.py
+++ exercise8_13.py
@@ -18,3 +18,15 @@ class Box(NamedTuple):
     @classmethod
     def randint(cls, a: int = 1, b: int = 256) -> "Box":
         return cls(*(randint(a, b) for _ in range(3)))
+
+
+def solve(stack: list[Box]) -> int:
+    boxes = [Box(0, 0, 0), *sorted(stack, key=tuple)]
+
+    highests = [0]
+    for box in boxes[1:]:
+        h = box.height
+        h_max = max(h + k for i, k in enumerate(highests) if boxes[i] < box)
+        highests.append(h_max)
+
+    return max(highests)
