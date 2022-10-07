313c236dfd28f7e43c23fbf8421f8feed0d56ba4
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-10-07 21:33:18 +0900

    10.6 partially done more

--- exercise10_6.py
+++ exercise10_6.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 from subprocess import run
+from tempfile import TemporaryDirectory
 from unittest import TestCase, main
 
 
-def solve(p: Path) -> None:
-    # Split files
-    run(["split", str(p)])
+def solve(p: Path, split_lines: int = 1000) -> None:
+    with TemporaryDirectory() as dirname:
+        # Split files
+        run(
+            ["split", "-l", str(split_lines), str(p.resolve())], cwd=dirname, check=True
+        )
 
-    # Sort each file
-    for file in ...:
-        run(["sort", "-o", file, file])
+        # Sort each file
+        d1r = Path(dirname)
+        for file in d1r.iterdir():
+            run(["sort", "-o", file, file], cwd=dirname, check=True)
 
-    # Merge sorted files
-    pass
+        # Merge sorted files
+        pass
 
 
 class TestSolution(TestCase):
