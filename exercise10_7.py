f8cddb03cfe65220c43409343efd774ea9ea7d8c
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-10-13 20:37:08 +0900

    10.7 solution done

---
+++ exercise10_7.py
@@ -0,0 +1,19 @@
+from csv import reader
+from pathlib import Path
+from typing import Optional
+from unittest import TestCase, main
+
+import numpy as np
+
+
+def solve(p: Path, max_int_digits=32, delimiter=",") -> Optional[int]:
+    missing = np.full(1 << max_int_digits, True)
+    with p.open() as f:
+        ints = reader(f, delimiter=delimiter)
+        for line in ints:
+            for s in line:
+                i = int(s)
+                missing[i] = False
+
+    idx = np.argmax(missing)
+    return idx if idx or missing[0] else None
