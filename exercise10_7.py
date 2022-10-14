3bf044fa140580ee9cb21ea4bf03bceabd1fec03
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-10-14 21:45:24 +0900

    10.7 small testcases done

--- exercise10_7.py
+++ exercise10_7.py
@@ -1,3 +1,4 @@
+from tempfile import TemporaryFile
 from typing import Optional, TextIO
 from unittest import TestCase, main
 
@@ -13,3 +14,25 @@ def solve(f: TextIO, max_int_digits=32) -> Optional[int]:
 
     idx = np.argmax(missing)
     return idx.item() if idx or missing[0] else None
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        it = 100
+        max_int_digits = 4
+        max_elem = 1 << max_int_digits
+        size = 2 * max_elem
+        with TemporaryFile("w+") as f:
+            for _ in range(it):
+                x = np.random.randint(max_elem, size=size)
+                np.savetxt(f, x, fmt="%d")
+                f.seek(0)
+                idx = solve(f, max_int_digits)
+                if len(set(x)) == max_elem:
+                    self.assertIsNone(idx)
+                else:
+                    self.assertNotIn(idx, x)
+
+
+if __name__ == "__main__":
+    main()
