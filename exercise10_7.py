8e2d876ee1e3e610fe864e1b83672b619c470151
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-10-14 21:44:54 +0900

    Simplify solve()'s interface

--- exercise10_7.py
+++ exercise10_7.py
@@ -1,19 +1,15 @@
-from csv import reader
-from pathlib import Path
-from typing import Optional
+from typing import Optional, TextIO
 from unittest import TestCase, main
 
 import numpy as np
 
 
-def solve(p: Path, max_int_digits=32, delimiter=",") -> Optional[int]:
+def solve(f: TextIO, max_int_digits=32) -> Optional[int]:
     missing = np.full(1 << max_int_digits, True)
-    with p.open() as f:
-        ints = reader(f, delimiter=delimiter)
-        for line in ints:
-            for s in line:
-                i = int(s)
-                missing[i] = False
+
+    for line in f:
+        i = int(line)
+        missing[i] = False
 
     idx = np.argmax(missing)
     return idx.item() if idx or missing[0] else None
