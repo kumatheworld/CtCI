aa9693e9fb3e3c1bbc20d2c05eb0fabfdaf58770
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-10-23 21:57:15 +0900

    10.10 test done

--- exercise10_10.py
+++ exercise10_10.py
@@ -1,5 +1,7 @@
 from bisect import bisect_right, insort
 from collections import UserList
+from random import randrange
+from unittest import TestCase, main
 
 from common import CT
 
@@ -13,3 +15,29 @@ class RankedSequence(UserList[CT]):
         if idx < 0 or self.data[idx] != x:
             raise ValueError(f"{x} is not in sequence")
         return idx
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        it = 100
+        size = 1000
+        for _ in range(it):
+            l = []
+            s = RankedSequence[int]()
+            for _ in range(size):
+                x = randrange(size)
+                y = randrange(size)
+                l.append(x)
+                s.track(x)
+                try:
+                    l.index(y)
+                except ValueError:
+                    with self.assertRaises(ValueError):
+                        s.get_rank(y)
+                else:
+                    rank = sum(1 for z in l if z <= y) - 1
+                    self.assertEqual(s.get_rank(y), rank)
+
+
+if __name__ == "__main__":
+    main()
