aedbc4939ae3146b5cf4c824ab8c3f28790f50e7
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-11-07 20:15:38 +0900

    Simplify 12.2

--- exercise12_2.cpp
+++ exercise12_2.cpp
@@ -1,9 +1,10 @@
 #include <cstring>
+#include <algorithm>
 
 void reverse(char *str)
 {
     int n = std::strlen(str);
-    int j = n;
+    std::reverse(str, str + n);
     for (int i = 0; i < n / 2; ++i)
     {
         --j;
