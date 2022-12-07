1ce8d15efb05be44ec9191989993adaf440b0038
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-12-07 22:43:01 +0900

    Change types from void* to char*

--- exercise12_10.c
+++ exercise12_10.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 
-void *align_malloc(size_t _Size, int div)
+char *align_malloc(size_t _Size, int div)
 {
-    void *p = malloc(_Size + div);
+    char *p = (char *)malloc(_Size + div);
     // Wanna free some initial segment of p, but is that possible?
     return p;
 }
 
-void aligned_free(void *_Memory)
+void aligned_free(char *_Memory)
 {
     free(_Memory);
 }
