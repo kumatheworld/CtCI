df80ee1a0c350bd35a9b02197af7abc01bdb9999
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-12-17 23:04:23 +0900

    Define Country

---
+++ exercise13_7.java
@@ -0,0 +1,17 @@
+class Country {
+    private String continent;
+    private int population;
+
+    public Country(String name) {
+        continent = "Imaginary";
+        population = 1000;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
