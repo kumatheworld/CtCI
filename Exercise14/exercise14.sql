56328b8e5323fd11a25a960f247a949bef1f8471
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-12-22 22:53:06 +0900

    CREATE DATABASE Exercise14

--- Exercise14/exercise14.sql
+++ Exercise14/exercise14.sql
@@ -1,6 +1,12 @@
-CREATE TABLE [dbo].[Apartments]
-(
-  AptID int,
-  UnitNumber varchar(100),
-  BuildingNumber int
+-- Create a new database called 'Exercise14'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+  SELECT name
+FROM sys.databases
+WHERE name = N'Exercise14'
 )
+CREATE DATABASE Exercise14
+GO
