2376e27ab52e60a427b5ea3cd6a3cb2d48f8cff8
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2022-12-26 23:10:00 +0900

    Create remaining tables

--- Exercise14/exercise14.sql
+++ Exercise14/exercise14.sql
@@ -31,3 +31,72 @@ CREATE TABLE ctci_exercise14.Apartment
   BuildingNumber INT
 );
 GO
+
+-- Create a new table called 'Buildings' in schema 'ctci_exercise14'
+-- Drop the table if it already exists
+IF OBJECT_ID('ctci_exercise14.Buildings', 'U') IS NOT NULL
+DROP TABLE ctci_exercise14.Buildings
+GO
+-- Create the table in the specified schema
+CREATE TABLE ctci_exercise14.Buildings
+(
+  BuildingID INT,
+  ComplexID INT,
+  BuildingName VARCHAR(100),
+  Address VARCHAR(500)
+);
+GO
+
+-- Create a new table called 'Requests' in schema 'ctci_exercise14'
+-- Drop the table if it already exists
+IF OBJECT_ID('ctci_exercise14.Requests', 'U') IS NOT NULL
+DROP TABLE ctci_exercise14.Requests
+GO
+-- Create the table in the specified schema
+CREATE TABLE ctci_exercise14.Requests
+(
+  RequestID INT,
+  Status VARCHAR(100),
+  AptID INT,
+  Description VARCHAR(500)
+);
+GO
+
+-- Create a new table called 'Complexes' in schema 'ctci_exercise14'
+-- Drop the table if it already exists
+IF OBJECT_ID('ctci_exercise14.Complexes', 'U') IS NOT NULL
+DROP TABLE ctci_exercise14.Complexes
+GO
+-- Create the table in the specified schema
+CREATE TABLE ctci_exercise14.Complexes
+(
+  ComplexID INT,
+  ComplexName VARCHAR(100)
+);
+GO
+
+-- Create a new table called 'AptTenants' in schema 'ctci_exercise14'
+-- Drop the table if it already exists
+IF OBJECT_ID('ctci_exercise14.AptTenants', 'U') IS NOT NULL
+DROP TABLE ctci_exercise14.AptTenants
+GO
+-- Create the table in the specified schema
+CREATE TABLE ctci_exercise14.AptTenants
+(
+  TenantID INT,
+  AptID INT
+);
+GO
+
+-- Create a new table called 'Tenants' in schema 'ctci_exercise14'
+-- Drop the table if it already exists
+IF OBJECT_ID('ctci_exercise14.Tenants', 'U') IS NOT NULL
+DROP TABLE ctci_exercise14.Tenants
+GO
+-- Create the table in the specified schema
+CREATE TABLE ctci_exercise14.Tenants
+(
+  TenantID INT,
+  TenantName VARCHAR(100)
+);
+GO
