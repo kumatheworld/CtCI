3a2fa091daeeb092687fb32399058c42b4c8202f
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2023-01-10 22:40:23 +0900

    16.8 test done?

---
+++ exercise16_8.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+
+
+def solve(n: int) -> str:
+    return ""
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        data = [
+            (0, "Zero"),
+            (12, "Twelve"),
+            (43, "Forty "),
+            (56, "Fifty Six"),
+            (789, "Seven Hundred Eighty Nine"),
+            (1234, "One Thousand, Two Hundred Thirty Four"),
+        ]
+        for n, s in data:
+            self.assertEqual(solve(n), s)
+
+
+if __name__ == "__main__":
+    main()
