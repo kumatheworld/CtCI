99d5567330508eb075371b9a4db54b028895f8b9
kum4th3w0rld@gmail.com <kum4th3w0rld@gmail.com>
2023-01-11 23:41:23 +0900

    16.8 done

--- exercise16_8.py
+++ exercise16_8.py
@@ -2,7 +2,68 @@ from unittest import TestCase, main
 
 
 def solve(n: int) -> str:
-    return ""
+    if n == 0:
+        return "Zero"
+
+    one_to_nineteen = [
+        "",
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
+        "Nineteen",
+    ]
+    twenty_to_Ninety = [
+        "",
+        "",
+        "Twenty",
+        "Thirty",
+        "Forty",
+        "Fifty",
+        "Sixty",
+        "Seventy",
+        "Eighty",
+        "Ninety",
+    ]
+    thousand_plus = ["Thousand", "Million", "Billion", "Trillion"]
+
+    l = []
+    i = 0
+    while True:
+        n, abc = divmod(n, 1000)
+        a, bc = divmod(abc, 100)
+        if bc:
+            if bc < 20:
+                l.append(one_to_nineteen[bc])
+            else:
+                b, c = divmod(bc, 10)
+                if c:
+                    l.append(one_to_nineteen[c])
+                l.append(twenty_to_Ninety[b])
+        if a:
+            l.append("Hundred")
+            l.append(one_to_nineteen[a])
+        if n:
+            l.append(thousand_plus[i] + ",")
+            i += 1
+        else:
+            break
+
+    return " ".join(reversed(l))
 
 
 class TestSolution(TestCase):
