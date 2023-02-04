d6afd83c17a1a7036ddb00859834d6e8eeec8ef6
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-04 22:46:30 +0800

    16.15 done

--- exercise16_15.py
+++ exercise16_15.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import Literal, TypeAlias
 from unittest import TestCase, main
 
@@ -6,13 +7,22 @@ Slots: TypeAlias = tuple[Color, Color, Color, Color]
 Hits: TypeAlias = Literal[0, 1, 2, 3]
 
 
-def solve(g: Slots, s2: Slots) -> tuple[Hits, Hits]:
-    return 0, 0
+def solve(g: Slots, s: Slots) -> tuple[Hits, Hits]:
+    h = sum(1 for c, d in zip(g, s) if c == d)
+    ph = sum((Counter(g) & Counter(s)).values()) - h
+    return h, ph
 
 
 class TestSolution(TestCase):
     def test(self) -> None:
-        pass
+        data = [
+            ((tuple("GGRR"), tuple("RGBY")), (1, 1)),
+            ((tuple("RGBY"), tuple("GGRR")), (1, 1)),
+            ((tuple("BYGG"), tuple("GRYB")), (0, 3)),
+            ((tuple("YBRB"), tuple("RYGB")), (1, 2)),
+        ]
+        for x, y in data:
+            self.assertEqual(solve(*x), y)
 
 
 if __name__ == "__main__":
