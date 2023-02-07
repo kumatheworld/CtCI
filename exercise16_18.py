81462bb44a25952645b44fba14874c79b2af1510
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-07 22:46:52 +0900

    16.18 testcases done

---
+++ exercise16_18.py
@@ -0,0 +1,25 @@
+from unittest import TestCase, main
+
+
+def solve(p: str, v: str) -> bool:
+    return True
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        data = [
+            (("aabab", "catcatgocatgo"), True),
+            (("a", "catcatgocatgo"), True),
+            (("ab", "catcatgocatgo"), True),
+            (("b", "catcatgocatgo"), True),
+            (("", "catcatgocatgo"), False),
+            (("aa", "catcatgocatgo"), False),
+            (("aba", "catcatgocatgo"), False),
+            (("bba", "catcatgocatgo"), False),
+        ]
+        for x, y in data:
+            self.assertEqual(solve(*x), y)
+
+
+if __name__ == "__main__":
+    main()
