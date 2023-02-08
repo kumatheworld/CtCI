9e091b1b0f8624cda5c87a2c0dee14bf02cd168e
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-08 22:54:53 +0900

    16.18 done

--- exercise16_18.py
+++ exercise16_18.py
@@ -1,8 +1,22 @@
+import re
 from unittest import TestCase, main
 
 
 def solve(p: str, v: str) -> bool:
-    return True
+    d = {}
+    l = []
+    i = 0
+    for c in p:
+        try:
+            l.append(f"\\{d[c]}")
+        except KeyError:
+            i += 1
+            d[c] = i
+            l.append("(.*)")
+    q = "".join(l)
+    r = re.compile(q)
+    m = re.fullmatch(r, v)
+    return bool(m)
 
 
 class TestSolution(TestCase):
