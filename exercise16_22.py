a010f9ac2c10713c80c93541b06ab92b103d46c1
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-15 22:46:33 +0900

    Make squares and expand() "private"

--- exercise16_22.py
+++ exercise16_22.py
@@ -4,8 +4,8 @@ from random import choices
 class Grid:
     def __init__(self) -> None:
         self.radius = 0
-        self.squares = []
+        self._squares = []
 
-    def expand(self) -> None:
+    def _expand(self) -> None:
         self.radius += 1
-        self.squares.append(choices([False, True], k=8 * self.radius))
+        self._squares.append(choices([False, True], k=8 * self.radius))
