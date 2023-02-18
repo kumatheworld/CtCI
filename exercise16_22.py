022729292e54f3226cf6e4e135eb00eccc99e5d6
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-18 22:55:54 +0900

    Add _translate_indices() instead of __getitem__()

--- exercise16_22.py
+++ exercise16_22.py
@@ -9,16 +9,27 @@ class Grid:
         self._squares = [[center]]
 
     def __str__(self) -> str:
-        # Could be faster by bypassing __getitem__
-        rng = range(-self.radius, self.radius + 1)
-        return "\n".join("".join("xo"[self[i, j]] for j in rng) for i in rng)
+        return self._str(org=False)
 
-    def __getitem__(self, ij: tuple[int, int]) -> bool:
+    def _str(self, org: bool) -> str:
+        # Could be faster by bypassing __getitem__
+        colors = "xo"
+        rng = range(-self.radius, self.radius + 1)
+        squares = self._squares_org if org else self._squares
+        l = []
+        for i in rng:
+            row = []
+            for j in rng:
+                r, t = self._translate_indices(i, j)
+                row.append(colors[squares[r][t]])
+            l.append("".join(row))
+        return "\n".join(l)
+
+    def _translate_indices(self, i: int, j: int) -> tuple[int, int]:
         #  0 ~ 2r-1: ( n,  n) ... (-n+1, n)
         # 2r ~ 4r-1: (-n,  n) ... (-n, -n+1)
         # 4r ~ 6r-1: (-n, -n) ... (n-1, -n)
         # 6r ~ 8r-1: ( n, -n) ... ( n, n-1)
-        i, j = ij
         r = max(abs(i), abs(j))
         if i <= j:
             if j == r:
@@ -30,7 +41,7 @@ class Grid:
                 t = 5 * r + i
             else:
                 t = 7 * r + j
-        return self._squares[r][t]
+        return r, t
 
     def _expand(self) -> None:
         self.radius += 1
