26586d3ad4ab56be0cc0cc64061b8d99bdee2b30
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-24 19:22:47 +0900

    16.26 done

--- exercise16_26.py
+++ exercise16_26.py
@@ -1,9 +1,34 @@
+import operator
+import re
 from random import choices, randrange
+from typing import Callable
 from unittest import TestCase, main
 
 
+def interpret_num_op_list(
+    num_op_list: list[str],
+    op_dict: dict[str, Callable[[float, float], float]],
+) -> str:
+    result = float(num_op_list[0])
+    op_list = num_op_list[1::2]
+    num_list = num_op_list[2::2]
+    for op, num in zip(op_list, num_list):
+        result = op_dict[op](result, float(num))
+    return str(result)
+
+
 def solve(s: str) -> float:
-    return 0
+    addsub = re.compile(r"([\+\-])")
+    muldiv = re.compile(r"([\*/])")
+    addsub_dict = {"+": operator.add, "-": operator.sub}
+    muldiv_dict = {"*": operator.mul, "/": operator.truediv}
+
+    terms = re.split(addsub, s)
+    terms[::2] = [
+        interpret_num_op_list(re.split(muldiv, t), muldiv_dict) for t in terms[::2]
+    ]
+    result = interpret_num_op_list(terms, addsub_dict)
+    return float(result)
 
 
 class TestSolution(TestCase):
