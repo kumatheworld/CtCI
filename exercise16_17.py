7d20435307e85ecf6d9f3355981dcf08ad09829b
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-25 22:46:31 +0900

    16.17 done

--- exercise16_17.py
+++ exercise16_17.py
@@ -3,13 +3,28 @@ from unittest import TestCase, main
 
 
 def solve(l: list[int]) -> int:
-    return 0
+    def solve_(offset: int, length: int) -> tuple[int, int, int, int]:
+        if length < 2:
+            s_ = l[offset] if length else 0
+            l_ = m_ = r_ = max(0, s_)
+            return s_, l_, m_, r_
+        h = length // 2
+        sl, ll, ml, rl = solve_(offset, h)
+        sr, lr, mr, rr = solve_(offset + h, length - h)
+        s_ = sl + sr
+        l_ = max(ll, sl + lr)
+        r_ = max(rl + sr, rr)
+        m_ = max(l_, ml, rl + lr, mr, r_)
+        return s_, l_, m_, r_
+
+    _, _, m, _ = solve_(0, len(l))
+    return m
 
 
 class TestSolution(TestCase):
     def test(self) -> None:
         iter = 1000
-        m = 10
+        m = 100
         p = range(-m, m)
         for _ in range(iter):
             k = randrange(m)
@@ -22,4 +37,5 @@ class TestSolution(TestCase):
 
 
 if __name__ == "__main__":
+    main()
     main()
