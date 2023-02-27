2e9e020d3227c619dc8649f92c7806d4db12c76d
kumatheworld <kum4th3w0rld@gmail.com>
2023-02-27 21:09:22 +0900

    Use SQLite, not T-SQL

--- exercise14.sql
+++ exercise14.sql
@@ -1,95 +1,35 @@
--- Create a new database called 'CtCIExercise14'
--- Connect to the 'master' database to run this snippet
-USE master
-GO
--- Create the new database if it does not exist already
-IF NOT EXISTS (
-  SELECT name
-FROM sys.databases
-WHERE name = N'CtCIExercise14'
-)
-CREATE DATABASE CtCIExercise14
-GO
-
--- Create a new table called 'Apartments' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.Apartments', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.Apartments
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.Apartments
-(
-  AptID INT,
-  UnitNumber VARCHAR(100),
-  BuildingNumber INT
+CREATE TABLE apartments (
+  apt_id INTEGER PRIMARY KEY,
+  unit_number TEXT,
+  building_number INTEGER
 );
-GO
 
--- Create a new table called 'Buildings' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.Buildings', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.Buildings
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.Buildings
-(
-  BuildingID INT,
-  ComplexID INT,
-  BuildingName VARCHAR(100),
-  Address VARCHAR(500)
+CREATE TABLE buildings (
+  building_id INTEGER PRIMARY KEY,
+  complex_id INTEGER,
+  building_name TEXT,
+  address TEXT
 );
-GO
 
--- Create a new table called 'Requests' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.Requests', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.Requests
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.Requests
-(
-  RequestID INT,
-  Status VARCHAR(100),
-  AptID INT,
-  Description VARCHAR(500)
+CREATE TABLE requests (
+  request_id INTEGER PRIMARY KEY,
+  status TEXT,
+  apt_id INTEGER,
+  description TEXT
 );
-GO
 
--- Create a new table called 'Complexes' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.Complexes', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.Complexes
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.Complexes
-(
-  ComplexID INT,
-  ComplexName VARCHAR(100)
+CREATE TABLE complexes (
+  complex_id INTEGER PRIMARY KEY,
+  complex_name TEXT
 );
-GO
 
--- Create a new table called 'AptTenants' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.AptTenants', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.AptTenants
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.AptTenants
-(
-  TenantID INT,
-  AptID INT
+CREATE TABLE apt_tenants (
+  tenant_id INTEGER,
+  apt_id INTEGER,
+  PRIMARY KEY (tenant_id, apt_id)
 );
-GO
 
--- Create a new table called 'Tenants' in schema 'dbo'
--- Drop the table if it already exists
-IF OBJECT_ID('CtCIExercise14.dbo.Tenants', 'U') IS NOT NULL
-DROP TABLE CtCIExercise14.dbo.Tenants
-GO
--- Create the table in the specified schema
-CREATE TABLE CtCIExercise14.dbo.Tenants
-(
-  TenantID INT,
-  TenantName VARCHAR(100)
+CREATE TABLE tenants (
+  tenant_id INTEGER PRIMARY KEY,
+  tenant_name TEXT
 );
-GO
