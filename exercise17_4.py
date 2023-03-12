bfe135fad82b611b55d99dce8a327021c8baa0b6
kumatheworld <kum4th3w0rld@gmail.com>
2023-03-12 20:22:07 +0900

    17.4 testcases done

---
+++ exercise17_4.py
@@ -0,0 +1,32 @@
+from random import randrange, shuffle
+from unittest import TestCase, main
+
+
+class MissingArray:
+    def __init__(self, i: int, n: int) -> None:
+        a = list(range(n + 1))
+        del a[i]
+        shuffle(a)
+        self._a = a
+
+    def query(self, i: int, j: int) -> int:
+        return (self._a[i] >> j) & 1
+
+
+def solve(a: MissingArray) -> int:
+    l = a._a
+    return (len(l) * (len(l) + 1)) // 2 - sum(l)
+
+
+class TestSolution(TestCase):
+    def test(self) -> None:
+        it = 1000
+        n = 1000
+        for _ in range(it):
+            i = randrange(n)
+            a = MissingArray(i, n)
+            self.assertEqual(solve(a), i)
+
+
+if __name__ == "__main__":
+    main()
