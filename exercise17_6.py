7fd2165f4a87721f629c296050b1130c4552e207
kumatheworld <kum4th3w0rld@gmail.com>
2023-03-26 22:54:48 +0900

    17.6 kinda done

--- exercise17_6.py
+++ exercise17_6.py
@@ -1,8 +1,16 @@
+from itertools import accumulate
 from unittest import TestCase, main
 
 
 def solve(n: int) -> int:
-    return 0
+    # Slower!
+    num_2s_above = accumulate(int(c == "2") for c in "0" + str(n)[:-1])
+    count = 0
+    for i, (d_str, n2b) in enumerate(reversed(list(zip(str(n), num_2s_above)))):
+        d = int(d_str)
+        tti = 10**i
+        count += n2b * d * tti + i * d * tti // 10 + (d > 2) * tti + (d == 2)
+    return count
 
 
 class TestSolution(TestCase):
