8ece53224581c294a6fa533e4fbd1861ecd76b02
kumatheworld <kum4th3w0rld@gmail.com>
2023-04-05 19:58:24 +0900

    Improve solution

--- exercise17_21.py
+++ exercise17_21.py
@@ -1,24 +1,38 @@
 from contextlib import suppress
+from itertools import pairwise
+from random import choices
 from unittest import TestCase, main
 
 from numpy import nonzero
 
 
 def solve(l: list[int]) -> int:
-    i = 1
-    vol = 0
-    with suppress(IndexError):
-        while True:
-            (above,) = nonzero([x >= i for x in l])
-            vol += above[-1] - above[0] + 1
-            i += 1
-    return vol - sum(l)
+    if not l:
+        return 0
+
+    s = sorted(set(l))
+    vol = s[0] * len(l) - sum(l)
+    for h, k in pairwise(s):
+        (above,) = nonzero([x >= k for x in l])
+        vol += (k - h) * (above[-1] - above[0] + 1)
+    return vol
 
 
 class TestSolution(TestCase):
     def test(self) -> None:
-        l = [0, 0, 4, 0, 0, 6, 0, 0, 3, 0, 5, 0, 1, 0, 0, 0]
-        self.assertEqual(solve(l), 26)
+        n = 100
+        for i in range(n):
+            l = choices(range(i), k=i)
+
+            j = 1
+            vol = -sum(l)
+            with suppress(IndexError):
+                while True:
+                    (above,) = nonzero([x >= j for x in l])
+                    vol += above[-1] - above[0] + 1
+                    j += 1
+
+            self.assertEqual(solve(l), vol)
 
 
 if __name__ == "__main__":
