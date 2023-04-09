0d6473ee7a924ae281a8951b9f1958660dc2aa79
kumatheworld <kum4th3w0rld@gmail.com>
2023-04-09 20:49:41 +0900

    17.24 testcases done

--- exercise17_24.py
+++ exercise17_24.py
@@ -1,31 +1,28 @@
-from itertools import chain, combinations, combinations_with_replacement, product
+from itertools import chain, combinations, combinations_with_replacement
 from unittest import TestCase, main
 
 import numpy as np
 
 
 def solve(m: np.ndarray) -> tuple[set[int], set[int]]:
-    return ([], [])
+    return (set(), set())
 
 
 class TestSolution(TestCase):
     def test(self) -> None:
         n = 5
-        it = 1
+        it = 100
         powerset = list(
             chain.from_iterable(combinations(range(n), i) for i in range(1, n + 1))
         )
-        print(powerset)
-        best_submatrix = set(), set()
         best_sum = 0
         for _ in range(it):
             m = 2 * n * np.random.rand(n, n) - n
-            print(m)
             for s, t in combinations_with_replacement(powerset, 2):
-                print(s, t)
-                if m[s, t].sum() > best_sum:
-                    best_submatrix = s, t
-        print(best_submatrix)
+                if (ss := m[np.ix_(s, t)].sum()) > best_sum:
+                    best_sum = ss
+        u, v = solve(m)
+        self.assertEqual(m[np.ix_(u, v)], best_sum)
 
 
 if __name__ == "__main__":
