50763292ec03724d2804cc85fc40a93f2f595e9d
kumatheworld <kum4th3w0rld@gmail.com>
2023-04-11 00:57:00 +0900

    Fix testcases

--- exercise17_19.py
+++ exercise17_19.py
@@ -9,9 +9,9 @@ def solve(l: list[int]) -> int:
 class TestSolution(TestCase):
     def test(self) -> None:
         n = 1000
-        for i in range(n):
-            l = sample(range(i + 1), i + 1)
-            self.assertEqual(solve(l[:1]), l[-1])
+        for i in range(1, n):
+            l = sample(range(i), i)
+            self.assertEqual(solve(l[:-1]), l[-1])
 
 
 if __name__ == "__main__":
