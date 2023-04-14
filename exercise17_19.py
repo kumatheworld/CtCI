c0e85486067ed57cf12cc25be15517bd81946cef
kumatheworld <kum4th3w0rld@gmail.com>
2023-04-14 22:52:54 +0800

    17.19 halfway

--- exercise17_19.py
+++ exercise17_19.py
@@ -1,9 +1,27 @@
+from itertools import islice
+from math import ceil, log2
 from random import sample
 from unittest import TestCase, main
 
 
 def solve(l: list[int]) -> int:
-    return 0
+    if not l:
+        return 0
+
+    n = len(l) + 1
+    offset = 0
+    stride = 1
+    digit = 0
+    for digit in range(ceil(log2(n))):
+        b = 1
+        for i in islice(l, offset, n, stride):
+            b ^= (i >> digit) & 1
+        if b:  # depends on the length? i'm confused...
+            offset += stride
+        stride <<= 1
+
+    print(l, offset)
+    return l[offset] ^ (1 << digit)
 
 
 class TestSolution(TestCase):
